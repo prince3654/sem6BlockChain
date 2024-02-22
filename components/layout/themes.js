e430f383814e349e9f6182d1550589f66a28576f
prince3654 <111579720+prince3654@users.noreply.github.com>
2024-02-22 21:12:49 +0530

    Add files via upload

---
+++ components/layout/themes.js
@@ -0,0 +1,22 @@
+const light = {
+    color: "#000",
+    bgColor: '#efe7fd',
+    bgImage: 'linear-gradient(180deg, #efe7f0 0%, #bffcf7 60%)',
+    bgDiv: '#fff',
+    bgSubDiv: '#efe7fd'
+}
+
+const dark = {
+    color: '#fff',
+    bgColor: '#03db5',
+    bgImage: 'linear-gradient(180deg, #3f0fdd 5%, #000000 90%)',
+    bgDiv: 'black',
+    bgSubDiv: 'rgb(33, 36, 41)'
+}
+
+const themes = {
+    light: light,
+    dark: dark,
+}
+
+export default themes;
